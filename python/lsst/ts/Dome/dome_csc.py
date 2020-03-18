beb24a2afa1ff7846e7a7e6d6b61a6701b2ef6bc
Wouter van Reeven <wvanreeven@lsst.org>
2020-03-18 15:32:38 -0300

    Reviewed the commands that need to be executed by this CSC and changed the CSC accordingly. Made sure that the unit tests pass.

--- python/lsst/ts/Dome/dome_csc.py
+++ python/lsst/ts/Dome/dome_csc.py
@@ -21,29 +21,19 @@ class DomeCsc(salobj.ConfigurableCsc):
         self.writer = None
         self.config = None
 
+        # Tasks that are run to execute specific functionality.
+        self.status_task = salobj.make_done_future()
+        self.connect_task = salobj.make_done_future()
+
         self.mock_ctrl = None  # mock controller, or None if not constructed
         super().__init__(
-            name="DCS",
+            name="Dome",
             index=0,
             schema_path=schema_path,
             config_dir=config_dir,
             initial_state=initial_state,
             simulation_mode=simulation_mode,
         )
-
-    async def do_crawlAz(self, data):
-        """
-        Crawl AZ
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
-
-    async def do_crawlEl(self, data):
-        """
-        Crawl El
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
 
     async def do_moveAz(self, data):
         """
@@ -55,55 +45,6 @@ class DomeCsc(salobj.ConfigurableCsc):
     async def do_moveEl(self, data):
         """
         Move El
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
-
-    async def do_park(self, data):
-        """
-        Park
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
-
-    async def do_setLouver(self, data):
-        """
-        Set Louver
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
-
-    async def do_closeLouvers(self, data):
-        """
-        Close Louvers
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
-
-    async def do_stopLouvers(self, data):
-        """
-        Stop Louvers
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
-
-    async def do_closeShutter(self, data):
-        """
-        Close Shutter
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
-
-    async def do_openShutter(self, data):
-        """
-        Open Shutter
-        """
-        self.assert_enabled()
-        raise salobj.ExpectedError("Not implemented")
-
-    async def do_stopShutter(self, data):
-        """
-        Stop Shutter
         """
         self.assert_enabled()
         raise salobj.ExpectedError("Not implemented")
@@ -129,6 +70,69 @@ class DomeCsc(salobj.ConfigurableCsc):
         self.assert_enabled()
         raise salobj.ExpectedError("Not implemented")
 
+    async def do_crawlAz(self, data):
+        """
+        Crawl AZ
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def do_crawlEl(self, data):
+        """
+        Crawl El
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def do_setLouver(self, data):
+        """
+        Set Louver
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def do_closeLouvers(self, data):
+        """
+        Close Louvers
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def do_stopLouvers(self, data):
+        """
+        Stop Louvers
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def do_openShutter(self, data):
+        """
+        Open Shutter
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def do_closeShutter(self, data):
+        """
+        Close Shutter
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def do_stopShutter(self, data):
+        """
+        Stop Shutter
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def do_park(self, data):
+        """
+        Park
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
     async def do_setTemperature(self, data):
         """
         Set Temperature
@@ -136,16 +140,54 @@ class DomeCsc(salobj.ConfigurableCsc):
         self.assert_enabled()
         raise salobj.ExpectedError("Not implemented")
 
+    async def config(self, data):
+        """
+        Internal Config command
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def fans(self, data):
+        """
+        Internal Fans command
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def inflate(self, data):
+        """
+        Internal Inflate command
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    async def status(self, data):
+        """
+        Internal Status command
+        """
+        self.assert_enabled()
+        raise salobj.ExpectedError("Not implemented")
+
+    @property
+    def connected(self):
+        if None in (self.reader, self.writer):
+            return False
+        return True
+
     @staticmethod
     def get_config_pkg():
         return "ts_config_mttcs"
 
     async def configure(self, config):
         self.config = config
-        # self.evt_settingsAppliedDomeTcp.set_put(
-        #     host=self.config.host,
-        #     port=self.config.port,
-        #     connectionTimeout=self.config.connection_timeout,
-        #     readTimeout=self.config.read_timeout,
-        #     force_output=True,
-        # )
+
+    async def implement_simulation_mode(self, simulation_mode):
+        if simulation_mode not in (0, 1):
+            raise salobj.ExpectedError(
+                f"Simulation_mode={simulation_mode} must be 0 or 1"
+            )
+
+    @classmethod
+    def add_kwargs_from_args(cls, args, kwargs):
+        super(DomeCsc, cls).add_kwargs_from_args(args, kwargs)
+        kwargs["simulation_mode"] = 1 if args.simulate else 0
--- tests/test_dome_csc.py
+++ tests/test_dome_csc.py
@@ -13,37 +13,38 @@ class CscTestCase(salobj.BaseCscTestCase, asynctest.TestCase):
             simulation_mode=simulation_mode,
         )
 
-    async def test_do_crawlAz(self):
-        pass
-
-    async def test_do_crawlEl(self):
-        pass
+    async def test_standard_state_transitions(self):
+        async with self.make_csc(
+            initial_state=salobj.State.STANDBY, config_dir=None, simulation_mode=1
+        ):
+            await self.check_standard_state_transitions(
+                enabled_commands=(
+                    "moveAz",
+                    "moveEl",
+                    "stopAz",
+                    "stopEl",
+                    "stop",
+                    "crawlAz",
+                    "crawlEl",
+                    "setLouver",
+                    "closeLouvers",
+                    "stopLouvers",
+                    "openShutter",
+                    "closeShutter",
+                    "stopShutter",
+                    "park",
+                    "setTemperature",
+                    "config",
+                    "fans",
+                    "inflate",
+                    "status",
+                )
+            )
 
     async def test_do_moveAz(self):
         pass
 
     async def test_do_moveEl(self):
-        pass
-
-    async def test_do_park(self):
-        pass
-
-    async def test_do_setLouver(self):
-        pass
-
-    async def test_do_closeLouvers(self):
-        pass
-
-    async def test_do_stopLouvers(self):
-        pass
-
-    async def test_do_closeShutter(self):
-        pass
-
-    async def test_do_openShutter(self):
-        pass
-
-    async def test_do_stopShutter(self):
         pass
 
     async def test_do_stopAz(self):
@@ -55,5 +56,44 @@ class CscTestCase(salobj.BaseCscTestCase, asynctest.TestCase):
     async def test_do_stop(self):
         pass
 
+    async def test_do_crawlAz(self):
+        pass
+
+    async def test_do_crawlEl(self):
+        pass
+
+    async def test_do_setLouver(self):
+        pass
+
+    async def test_do_closeLouvers(self):
+        pass
+
+    async def test_do_stopLouvers(self):
+        pass
+
+    async def test_do_openShutter(self):
+        pass
+
+    async def test_do_closeShutter(self):
+        pass
+
+    async def test_do_stopShutter(self):
+        pass
+
+    async def test_do_park(self):
+        pass
+
     async def test_do_setTemperature(self):
+        pass
+
+    async def test_config(self):
+        pass
+
+    async def test_fans(self):
+        pass
+
+    async def test_inflate(self):
+        pass
+
+    async def test_status(self):
         pass
